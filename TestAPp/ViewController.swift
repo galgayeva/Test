0b9f8e94a8f4f0df323c9a531f71284deac1ac7d
galgayeva <gunay.algayeva@pashabank.az>
2022-11-22 11:31:36 +0400

    test

---
+++ TestAPp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAPp
+//
+//  Created by Gunay Algayeva on 22.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
